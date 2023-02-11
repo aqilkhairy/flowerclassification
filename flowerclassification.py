7cafca5151baddbe1613862d59e0b4778d3d8f38
aqilkhairy <aqilkhairy@gmail.com>
2023-02-11 19:19:35 +0800

    latest

--- flowerclassification.py
+++ flowerclassification.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 import tkinter as tk
 import numpy as np
 from tensorflow.keras.models import load_model
@@ -12,18 +13,31 @@ def load_predict():
 
     # load the image and preprocess it
     image = Image.open(file_name)
-    image = image.resize((224, 224))
+    image = image.resize((180, 180))
     image = np.array(image) / 255.0
     image = np.expand_dims(image, axis=0)
 
     # use the model to make a prediction
     prediction = model.predict(image)
+    
+    #get score
+    score = tf.nn.softmax(prediction[0])
 
+    # define the list of class names
+    class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
+    
+    # get the class index with the highest probability
+    class_index = np.argmax(score)
+    
+    # get the class name corresponding to the class index
+    class_name = class_names[class_index]
+    
     # display the image and the prediction in the GUI
     image = ImageTk.PhotoImage(Image.open(file_name))
     label_image.config(image=image)
     label_image.image = image
-    label_prediction.config(text=str(prediction))
+    label_prediction.config(text=class_name)
+    label_predictionArray.config(text=str(np.max(score) * 100))
 
 # create the GUI
 root = tk.Tk()
@@ -35,10 +49,13 @@ entry.pack()
 button = tk.Button(root, text="Predict", command=load_predict)
 button.pack()
 
-label_image = tk.Label(root, text="")
+label_image = tk.Label(root, text="Label Image")
 label_image.pack()
 
-label_prediction = tk.Label(root, text="")
+label_prediction = tk.Label(root, text="Label Prediction")
 label_prediction.pack()
+
+label_predictionArray = tk.Label(root, text="Label Prediction Score")
+label_predictionArray.pack()
 
 root.mainloop()
